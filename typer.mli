dba4fcce423c0b2bb9b8d3b95f2e399e06417fbe
Raphaël Cauderlier <cauderlier@crans.org>
2014-12-05 17:58:17 +0100

    Missing option in typer.mli

--- typer.mli
+++ typer.mli
@@ -17,4 +17,4 @@ val declare_constant : (string * Expr.expr) -> unit
 val print_constant_decls : out_channel -> unit
 
 (* Type-check a list of phrases, the bool annotation is passed unchanged *)
-val phrasebl : (Phrase.phrase * bool) list -> (Phrase.phrase * bool) list
+val phrasebl : opts -> (Phrase.phrase * bool) list -> (Phrase.phrase * bool) list
