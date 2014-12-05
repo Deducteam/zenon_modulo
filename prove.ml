Deducteam/zenon_modulo

commit 0423c5413d7557553294476c04a78131e5a13131

Equality symbols are generated with correct type

prove.ml
@@ -129,7 +129,7 @@ let rec make_inequals_aux l1 l2 =
   match l1, l2 with
   | [], [] -> []
   | h1::t1, h2::t2 ->
-      [enot (eapp (eeq, [h1; h2]))] :: make_inequals_aux t1 t2
+      [enot (eeq h1 h2)] :: make_inequals_aux t1 t2
   | _, _ -> assert false
 ;;
 let make_inequals l1 l2 = Array.of_list (make_inequals_aux l1 l2);;
@@ -336,7 +336,7 @@ let newnodes_eq_str st fm g _ =
       nrule = Ext ("", rule, [e1; s1; e2; s2]);
       nprio = Prop;
       ngoal = g;
-      nbranches = [| [enot (eapp (eeq, [e1; e2]))] |];
+      nbranches = [| [enot (eeq e1 e2)] |];
     }, false
   in
   match fm with
@@ -345,12 +345,12 @@ let newnodes_eq_str st fm g _ =
      let r = Index.find_str_eq () in
      let fl st (e2, s) =
        let s2 = eapp (evar "$string", [evar s]) in
-       let eq = eapp (eeq, [e2; s2]) in
+       let eq = eeq e2 s2 in
        mk_node st "stringdiffll" e1 s1 e2 s2 eq
      in
      let fr st (e2, s) =
        let s2 = eapp (evar "$string", [evar s]) in
-       let eq = eapp (eeq, [s2; e2]) in
+       let eq = eeq s2 e2 in
        mk_node st "stringdifflr" e1 s1 e2 s2 eq
      in
      List.fold_left fr (List.fold_left fl (st, false) l) r
@@ -664,7 +664,7 @@ let newnodes_refl st fm g _ =
         nrule = Refl (s', e1, e2);
         nprio = Arity;
         ngoal = g;
-        nbranches = [| [enot (eapp (eeq, [e1; e2]))] |];
+        nbranches = [| [enot (eeq e1 e2)] |];
       }, false
 
   | Enot (Eapp (Evar("=",_), [Emeta (m1, _) as e1; Emeta (m2, _) as e2], _), _) ->
@@ -715,8 +715,8 @@ let mknode_trans sym (e1, g1) (e2, g2) =
   in
   let (x, y, z, t) = if sym then (d, a, b, c) else (c, a, b, d) in
   let branches = [|
-    [enot (eapp (eeq, [x; y])); enot (eapp (r', [x; y]))];
-    [enot (eapp (eeq, [z; t])); enot (eapp (r', [z; t]))];
+    [enot (eeq x y); enot (eapp (r', [x; y]))];
+    [enot (eeq z t); enot (eapp (r', [z; t]))];
   |] in
   {
     nconc = [e1; e2];
@@ -742,9 +742,9 @@ let mknode_transeq sym (e1, g1) (e2, g2) =
     else (c, a, b, d)
   in
   let branches = [|
-    [enot (eapp (eeq, [x; y])); enot (eapp (r', [x; y]))];
+    [enot (eeq x y); enot (eapp (r', [x; y]))];
     [enot (eapp (r', [x; y])); enot (eapp (r', [z; t]))];
-    [enot (eapp (eeq, [z; t])); enot (eapp (r', [z; t]))];
+    [enot (eeq z t); enot (eapp (r', [z; t]))];
   |] in
   {
     nconc = [e1; e2];
@@ -837,10 +837,10 @@ let newnodes_match_trans st fm g _ =
         ] in
         let eqnodes =
           if s' =%= "=" then [] else
-          let eqmatches_ll = Index.find_trans_left eeq h1 in
-          let eqmatches_rr = Index.find_trans_right eeq h2 in
-          let eqmatches_lr = Index.find_trans_right eeq h1 in
-          let eqmatches_rl = Index.find_trans_left eeq h2 in
+          let eqmatches_ll = Index.find_trans_left s h1 in
+          let eqmatches_rr = Index.find_trans_right s h2 in
+          let eqmatches_lr = Index.find_trans_right s h1 in
+          let eqmatches_rl = Index.find_trans_left s h2 in
           List.flatten [
             List.map (mknode_negtranseq false fmg) eqmatches_ll;
             List.map (mknode_negtranseq true fmg) eqmatches_lr;

coqterm.ml
@@ -218,11 +218,11 @@ let rec trtree env node =
       let lamp = mklam p subp in
       Clet (getname (enot p), lamp, subnp)
   | Rnoteq (e) ->
-      let e_neq_e = getv env (enot (eapp (eeq, [e; e]))) in
+      let e_neq_e = getv env (enot (eeq e e)) in
       Capp (Cvar "zenon_noteq", [Cwild; trexpr e; e_neq_e])
   | Reqsym (e, f) ->
-      let e_eq_f = getv env (eapp (eeq, [e; f])) in
-      let f_neq_e = getv env (enot (eapp (eeq, [f; e]))) in
+      let e_eq_f = getv env (eeq e f) in
+      let f_neq_e = getv env (enot (eeq f e)) in
       Capp (Cvar "zenon_eqsym", [Cwild; trexpr e; trexpr f; e_eq_f; f_neq_e])
   | Rnotnot (p) ->
       let sub = mklam p (tr_subtree_1 hyps) in
@@ -316,12 +316,12 @@ let rec trtree env node =
   | Rpnotp _ -> assert false
   | Rnotequal ((Eapp (Evar(f,_) as f', args1, _) as ff), (Eapp (Evar(g,_), args2, _) as gg)) ->
      assert (f = g);
-     let gen x = enot (eapp (eeq, [eapp (f', x); gg])) in
+     let gen x = enot (eeq (eapp (f', x)) gg) in
      let args = mk_eq_args gen [] args1 args2 in
      let base = Capp (Cvar "zenon_notnot",
                       [Cwild; Capp (Cvar "refl_equal", [trexpr gg])])
      in
-     let neq = enot (eapp (eeq, [ff; gg])) in
+     let neq = enot (eeq ff gg) in
      Capp (List.fold_right2 (mk_eq_node env) args hyps base, [getv env neq])
   | Rnotequal _ -> assert false
   | RcongruenceLR (p, a, b) ->
@@ -329,7 +329,7 @@ let rec trtree env node =
      let h = apply p b in
      let lam = mklam h sub in
      let concl1 = getv env (apply p a) in
-     let concl2 = getv env (eapp (eeq, [a; b])) in
+     let concl2 = getv env (eeq a b) in
      Capp (Cvar "zenon_congruence_lr",
            [Cwild; trexpr p; trexpr a; trexpr b; lam; concl1; concl2])
   | RcongruenceRL (p, a, b) ->
@@ -337,7 +337,7 @@ let rec trtree env node =
      let h = apply p b in
      let lam = mklam h sub in
      let concl1 = getv env (apply p a) in
-     let concl2 = getv env (eapp (eeq, [b; a])) in
+     let concl2 = getv env (eeq b a) in
      Capp (Cvar "zenon_congruence_rl",
            [Cwild; trexpr p; trexpr a; trexpr b; lam; concl1; concl2])
   | Rdefinition (name, sym, args, body, None, folded, unfolded) ->
@@ -355,7 +355,7 @@ let rec trtree env node =
   | Rextension (_, "zenon_induct_discriminate_diff",
                 [], [a; b; car], []) ->
      let subp = tr_subtree_1 hyps in
-     let h = enot (eapp (eeq, [a; b])) in
+     let h = enot (eeq a b) in
      Clet (getname h, Capp (Cvar "eq_ind",
                             [trexpr a; trexpr car; Cvar "I"; trexpr b]),
            subp)
@@ -368,8 +368,7 @@ let rec trtree env node =
        let shape =
          let vvars = List.map evar vars in
          let params = List.map (fun _ -> evar "_") args in
-         let base = enot (eapp (eeq,
-                                [e1; eapp (evar "@", evar c :: params @ vvars)]))
+         let base = enot (eeq e1 (eapp (evar "@", evar c :: params @ vvars)))
          in
          enot (all_list vvars base)
        in
@@ -386,7 +385,7 @@ let rec trtree env node =
      let recargs = List.map2 make_hyp hyps cstrs in
      let pred =
        let v = Expr.newvar () in
-       trexpr (elam (v, enot (eapp (eeq, [e1; v]))))
+       trexpr (elam (v, enot (eeq e1 v)))
      in
      let refl = Capp (Cvar "refl_equal", [tropt e1]) in
      Capp (Cvar schema, typargs @ pred :: recargs @ tropt e1 :: [refl])
@@ -440,7 +439,7 @@ and mk_eq_node env (ctx, a, b) h sub =
   if Expr.equal a b then sub else begin
     let x = Expr.newname () in
     let c = Clam (x, Cwild, trexpr env (ctx (evar x))) in
-    let aneb = enot (eapp (eeq, [a; b])) in
+    let aneb = enot (eeq a b) in
     let thyp = mklam env aneb (trtree env h) in
     Capp (Cvar "zenon_subst",
           [Cwild; c; trexpr env a; trexpr env b; thyp; sub])

eqrel.ml
@@ -230,7 +230,7 @@ let subsumed_subexpr e (path, env, sb, typ) =
 let subsumed e = List.for_all (subsumed_subexpr e) (get_subexprs e);;
 
 let eq_origin = Some (etrue, [], []);;
-Hashtbl.add tbl (eeq) {
+Hashtbl.add tbl (evar "=") {
   refl = eq_origin;
   sym = eq_origin;
   trans = eq_origin;

expr.ml
@@ -296,6 +296,40 @@ let priv_tau v e =
          (get_type v)
 ;;
 
+(************************)
+(* temp printers *)
+let rec print b ex =
+  match ex with
+  | Evar (v, _) -> Printf.bprintf b "%s" v;
+  | Emeta (e, _) -> Printf.bprintf b "M.(%a)" print e;
+  | Earrow (args, ret, _) ->
+      Printf.bprintf b "(%a -> %a)"
+      (fun b l -> List.iteri (fun i x -> if i > 0 then Printf.bprintf b " * %a" print x) l) args print ret;
+  | Eapp (s, es, _) ->
+      Printf.bprintf b "(%a%a)" print s
+      (fun b l -> List.iter (fun x -> Printf.bprintf b " %a" print x) l) es
+  | Enot (e, _) ->
+      Printf.bprintf b "~ %a" print e
+  | Eand (e1, e2, _) ->
+      Printf.bprintf b "(%a /\\ %a)" print e1 print e2
+  | Eor (e1, e2, _) ->
+      Printf.bprintf b "(%a \\/ %a)" print e1 print e2
+  | Eimply (e1, e2, _) ->
+      Printf.bprintf b "(%a => %a)" print e1 print e2
+  | Eequiv (e1, e2, _) ->
+      Printf.bprintf b "(%a <=> %a)" print e1 print e2
+  | Etrue -> Printf.bprintf b "True";
+  | Efalse -> Printf.bprintf b "False";
+  | Eall (v, e, _) ->
+      Printf.bprintf b "(A.(%a : %a): %a)" print v print (get_type v) print e
+  | Eex (v, e, _) ->
+      Printf.bprintf b "(E.(%a : %a): %a)" print v print (get_type v) print e
+  | Etau (v, e, _) ->
+      Printf.bprintf b "(T.(%a : %a): %a)" print v print (get_type v) print e
+  | Elam (v, e, _) ->
+      Printf.bprintf b "(L.(%a : %a): %a)" print v print (get_type v) print e
+
+
 module HashedExpr = struct
   type t = expr;;
 
@@ -464,7 +498,6 @@ let priv_lam v e =
 let elam (v, e) = he_merge (Elam (v, e, priv_lam v e))
 ;;
 
-let eeq = evar "=";;
 let estring = evar "$string";;
 
 let rec all_list vs body =
@@ -576,6 +609,8 @@ let rec priv_app s args =
   let sz = List.fold_left (fun a e -> a + get_size e) 1 args in
   let taus = List.fold_left (fun a e -> max (get_taus e) a) 0 args in
   let metas = List.fold_left (fun a e -> union (get_metas e) a) [] args in
+  Log.debug 15 "Typing '%a'" print s;
+  List.iter (fun x -> Log.debug 15 " |- %a ::: %a" print x print (get_type x)) args;
   let typ = type_app (get_type s) args in
   mkpriv skel fv sz taus metas typ
 
@@ -608,6 +643,11 @@ and type_app s args =
     | t, [] -> t
     | _ -> assert false
 
+and eeq a b =
+    let t = get_type a in
+    let s = tvar "=" (earrow [t; t] type_prop) in
+    eapp (s, [a; b])
+
 and substitute map e =
   let aux f map v body =
       let t = substitute map (get_type v) in
@@ -628,6 +668,7 @@ and substitute map e =
   | Evar (v, _) -> (try List.assq e map with Not_found -> e)
   | Emeta _ -> e
   | Earrow(args, ret, _) -> earrow (List.map (substitute map) args) (substitute map ret)
+  | Eapp (Evar ("=", _), [a; b], _) -> eeq (substitute map a) (substitute map b)
   | Eapp (s, args, _) -> eapp (s, List.map (substitute map) args)
   | Enot (f, _) -> enot (substitute map f)
   | Eand (f, g, _) -> eand (substitute map f, substitute map g)

expr.mli
@@ -50,7 +50,7 @@ val tvar : string -> expr -> expr;;
 val emeta : expr -> expr;;
 val eapp : expr * expr list -> expr;;
 
-val eeq : expr;;
+val eeq : expr -> expr -> expr;;
 val estring : expr;;
 
 (* Prop construction *)

ext_focal.ml
@@ -132,7 +132,7 @@ let newnodes_istrue e g =
         nbranches = branches;
       }; Stop ]
   | Eapp (Evar(op,_), [e1; e2; e3], _) when is_true_equal op ->
-     let branches = [| [eapp (eeq, [e2; e3])] |] in
+     let branches = [| [eeq e2 e3] |] in
      let name = chop_prefix "Is_true**" op in
        [ Node {
          nconc = [e];
@@ -178,7 +178,7 @@ let newnodes_istrue e g =
         nbranches = branches;
       }; Stop ]
   | Enot (Eapp (Evar(op,_), [e1; e2; e3], _), _) when is_true_equal op ->
-     let branches = [| [enot (eapp (eeq, [e2; e3]))] |] in
+     let branches = [| [enot (eeq e2 e3)] |] in
      let name = chop_prefix "Is_true**" op in
        [ Node {
          nconc = [e];
@@ -282,7 +282,7 @@ let newnodes_istrue e g =
           nrule = Ext ("focal", "istrue_true", [e1]);
           nprio = Arity;
           ngoal = g;
-          nbranches = [| [eapp (eeq, [e1; evar "true"])] |];
+          nbranches = [| [eeq e1 (evar "true")] |];
       } ]
   | Enot (Eapp (Evar("Is_true",_), [Eapp (Evar("$fix",_), _, _) as e1], _), _) ->
       [ Node {
@@ -290,7 +290,7 @@ let newnodes_istrue e g =
           nrule = Ext ("focal", "notistrue_false", [e1]);
           nprio = Arity;
           ngoal = g;
-          nbranches = [| [eapp (eeq, [e1; evar "false"])] |];
+          nbranches = [| [eeq e1 (evar "false")] |];
       } ]
   | Eapp (Evar("Is_true",_), [Eapp (Evar(s,_), args, _)], _) when Index.has_def s ->
      let ctx x = eapp (evar "Is_true", [x]) in
@@ -439,7 +439,7 @@ let to_llargs tr_expr r =
       let c = tr_expr (istrue (eapp (evar "coq_builtins.bi__not_b", [e1]))) in
       ("zenon_focal_not", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "equal", [Evar (name, _)as a; e1; e2; e3]) ->
-      let h = tr_expr (eapp (eeq, [e2; e3])) in
+      let h = tr_expr (eeq e2 e3) in
       let c = tr_expr (istrue (eapp (a, [e1; e2; e3]))) in
       let eqdec = evar ("zenon_focal_eqdec") in
       (name_of_equality_lemma,
@@ -461,7 +461,7 @@ let to_llargs tr_expr r =
       let c = tr_expr (enot (istrue (eapp (evar "coq_builtins.bi__not_b", [e1])))) in
       ("zenon_focal_notnot", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "notequal", [Evar (name, _) as a; e1; e2; e3]) ->
-      let h = tr_expr (enot (eapp (eeq, [e2; e3]))) in
+      let h = tr_expr (enot (eeq e2 e3)) in
       let c = tr_expr (enot (istrue (eapp (a, [e1; e2; e3])))) in
       (name_of_notequality_lemma,
        [tr_expr e1; tr_expr e2; tr_expr e3], [c], [ [h] ])
@@ -472,35 +472,35 @@ let to_llargs tr_expr r =
       let c = tr_expr (enot (istrue (evar "true"))) in
       ("zenon_focal_nottrue", [], [c], []);
   | Ext (_, "trueequal", [e1]) ->
-     let c = tr_expr (eapp (eeq, [evar "true"; e1])) in
+     let c = tr_expr (eeq (evar "true") e1) in
      let h = tr_expr (eapp (evar "Is_true", [e1])) in
      ("zenon_focal_trueequal", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "equaltrue", [e1]) ->
-     let c = tr_expr (eapp (eeq, [e1; evar "true"])) in
+     let c = tr_expr (eeq e1 (evar "true")) in
      let h = tr_expr (eapp (evar "Is_true", [e1])) in
      ("zenon_focal_equaltrue", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "truenotequal", [e1]) ->
-     let c = tr_expr (enot (eapp (eeq, [evar "true"; e1]))) in
+     let c = tr_expr (enot (eeq (evar "true") e1)) in
      let h = tr_expr (enot (eapp (evar "Is_true", [e1]))) in
      ("zenon_focal_truenotequal", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "notequaltrue", [e1]) ->
-     let c = tr_expr (enot (eapp (eeq, [e1; evar "true"]))) in
+     let c = tr_expr (enot (eeq e1 (evar "true"))) in
      let h = tr_expr (enot (eapp (evar "Is_true", [e1]))) in
      ("zenon_focal_notequaltrue", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "falseequal", [e1]) ->
-     let c = tr_expr (eapp (eeq, [evar "false"; e1])) in
+     let c = tr_expr (eeq (evar "false") e1) in
      let h = tr_expr (enot (eapp (evar "Is_true", [e1]))) in
      ("zenon_focal_falseequal", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "equalfalse", [e1]) ->
-     let c = tr_expr (eapp (eeq, [e1; evar "false"])) in
+     let c = tr_expr (eeq e1 (evar "false")) in
      let h = tr_expr (enot (eapp (evar "Is_true", [e1]))) in
      ("zenon_focal_equalfalse", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "falsenotequal", [e1]) ->
-     let c = tr_expr (enot (eapp (eeq, [evar "false"; e1]))) in
+     let c = tr_expr (enot (eeq (evar "false") e1)) in
      let h = tr_expr (eapp (evar "Is_true", [e1])) in
      ("zenon_focal_falsenotequal", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "notequalfalse", [e1]) ->
-     let c = tr_expr (enot (eapp (eeq, [e1; evar "false"]))) in
+     let c = tr_expr (enot (eeq e1 (evar "false"))) in
      let h = tr_expr (eapp (evar "Is_true", [e1])) in
      ("zenon_focal_notequalfalse", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "merge", _) -> ("zenon_focal_merge", [], [], [])
@@ -575,11 +575,11 @@ let to_llargs tr_expr r =
       ("zenon_focal_ite_rel_nr", List.map tr_expr [rf; e1; c; t; e],
        [concl], [ [ht1; ht2]; [he1; he2] ])
   | Ext (_, "istrue_true", [e1]) ->
-     let h = tr_expr (eapp (eeq, [e1; evar "true"])) in
+     let h = tr_expr (eeq e1 (evar "true")) in
      let c = tr_expr (istrue e1) in
      ("zenon_focal_istrue_true", [tr_expr e1], [c], [ [h] ])
   | Ext (_, "notistrue_false", [e1]) ->
-     let h = tr_expr (eapp (eeq, [e1; evar "false"])) in
+     let h = tr_expr (eeq e1 (evar "false")) in
      let c = tr_expr (enot (istrue e1)) in
      ("zenon_focal_notistrue_false", [tr_expr e1], [c], [ [h] ])
   | Ext (x, y, _) ->
@@ -763,7 +763,7 @@ let rec process_prooftree p =
       let fa1 = eapp (evar s, List.map process_expr args1) in
       let fa2 = eapp (evar s, List.map process_expr args2) in
       let step1 = {
-        conc = Expr.union [enot (eapp (eeq, [fa1; fa2]))] pconc;
+        conc = Expr.union [enot (eeq fa1 fa2)] pconc;
         rule = Rnotequal (fa1, fa2);
         hyps = phyps;
       } in

ext_induct.ml
@@ -198,7 +198,7 @@ let make_match_branches e cases =
           | _ -> eapp (constr, rvars)
         in
 *)
-        let shape = enot (eapp (eeq, [e; pattern])) in
+        let shape = enot (eeq e pattern) in
         [enot (all_list rvars shape)]
       in
       List.map f c
@@ -225,7 +225,7 @@ let make_match_congruence e g ctx is_t ee cases rhs =
   let key = if is_t then "Is_true**$match" else "$match" in
   let p = elam (x, ctx (eapp (evar key, x :: cases))) in
   Node {
-    nconc = [e; eapp (eeq, [ee; rhs])];
+    nconc = [e; eeq ee rhs];
     nrule = CongruenceLR (p, ee, rhs);
     nprio = Arity;
     ngoal = g;
@@ -376,7 +376,7 @@ let newnodes_injective e g =
        let args2 = get_args e2 in
        let ty = evar ((Hashtbl.find constructor_table (get_constr e1)).cd_type)
        in
-       let branch = List.map2 (fun x y -> eapp (eeq, [x; y])) args1 args2 in
+       let branch = List.map2 (fun x y -> eeq x y) args1 args2 in
        [ Node {
          nconc = [e];
          nrule = Ext ("induct", "injection", [e; ty]);
@@ -412,7 +412,7 @@ let newnodes_injective e g =
      let cas2 = make_case_expr "_" [] efalse in
      let x = newvar () in
      let caract = elam (x, eapp (evar "$match", [x; cas1; cas2])) in
-     let h = enot (eapp (eeq, [e2; e1])) in
+     let h = enot (eeq e2 e1) in
      [ Node {
       nconc = [];
        nrule = Ext ("induct", "discriminate_diff", [e2; e1; caract]);
@@ -433,7 +433,7 @@ let newnodes_injective e g =
      in
      if is_multiconstr then begin
        let any = eapp (evar "$any-induct", [evar (desc.cd_type); e1]) in
-       let h = enot (eapp (eeq, [e1; any])) in
+       let h = enot (eeq e1 any) in
        [ Node {
          nconc = [];
          nrule = Ext ("induct", "discriminate_meta", []);
@@ -452,7 +452,7 @@ let newnodes_constr_eq e g =
     let mk_node accu ee =
       if Expr.equal e0 ee then accu
       else begin
-        let eqn = eapp (eeq, [e0; ee]) in
+        let eqn = eeq e0 ee in
         Node {
           nconc = [];
           nrule = Cut eqn;
@@ -606,7 +606,7 @@ let to_llproof tr_expr mlp args =
        match args1, args2 with
        | [], [] -> accu
        | a1 :: t1, a2 :: t2 ->
-          let hyp = tr_expr (eapp (eeq, [a1; a2])) in
+          let hyp = tr_expr (eeq a1 a2) in
           if List.exists (Expr.equal hyp) accu.conc then begin
             let (_, cons, schema) =
               try Hashtbl.find type_table ty with Not_found -> assert false
@@ -646,7 +646,7 @@ let to_llproof tr_expr mlp args =
        match args1, args2 with
        | [], [] -> accu
        | a1 :: t1, a2 :: t2 ->
-          let hyp = tr_expr (eapp (eeq, [a1; a2])) in
+          let hyp = tr_expr (eeq a1 a2) in
           if List.exists (Expr.equal hyp) accu.conc then begin
             let (_, cons, schema) =
               try Hashtbl.find type_table ty with Not_found -> assert false
@@ -875,7 +875,7 @@ let p_rule_coq oc r =
   | Rextension (_, "zenon_induct_discriminate", [], [conc; _], []) ->
       poc "discriminate %s.\n" (getname conc);
   | Rextension (_, "zenon_induct_discriminate_diff", [], [e1; e2; _], []) ->
-      let h = enot (eapp (eeq, [e1; e2])) in
+      let h = enot (eeq e1 e2) in
       poc "assert (%a) as %s. discriminate.\n" p_expr h (getname h);
   | Rextension (_, "zenon_induct_cases", [Evar (ty, _); ctx; e1], [c], hs) ->
      poc "case_eq (%a); [\n    " p_expr e1;

commit 962f0fea6ec2c4ff3ed81e897bcab0035a6a846f

Fixed a bug introduced in 0423c541

prove.ml
@@ -733,7 +733,10 @@ let mknode_transeq sym (e1, g1) (e2, g2) =
   let (r', r, a, b, c, d) =
     match e1, e2 with
     | Eapp (Evar("=",_), [a; b], _), Enot (Eapp (Evar(r,_) as r', [c; d], _), _) -> (r', r, a, b, c, d)
-    | _, _ -> assert false
+    | _, _ ->
+            Log.debug 0 "e1 : %a" Print.pp_expr e1;
+            Log.debug 0 "e2 : %a" Print.pp_expr e2;
+            assert false
   in
   let rsym = Eqrel.sym r' in
   let (x, y, z, t) =
@@ -837,10 +840,12 @@ let newnodes_match_trans st fm g _ =
         ] in
         let eqnodes =
           if s' =%= "=" then [] else
-          let eqmatches_ll = Index.find_trans_left s h1 in
-          let eqmatches_rr = Index.find_trans_right s h2 in
-          let eqmatches_lr = Index.find_trans_right s h1 in
-          let eqmatches_rl = Index.find_trans_left s h2 in
+          let eq1 = tvar "=" (earrow [get_type e1; get_type e1] type_prop) in
+          let eq2 = tvar "=" (earrow [get_type e2; get_type e2] type_prop) in
+          let eqmatches_ll = Index.find_trans_left eq1 h1 in
+          let eqmatches_rr = Index.find_trans_right eq2 h2 in
+          let eqmatches_lr = Index.find_trans_right eq1 h1 in
+          let eqmatches_rl = Index.find_trans_left eq2 h2 in
           List.flatten [
             List.map (mknode_negtranseq false fmg) eqmatches_ll;
             List.map (mknode_negtranseq true fmg) eqmatches_lr;