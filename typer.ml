988c91e858aff8d58c20cf23a730f540eed11d40
Raphaël Cauderlier <cauderlier@crans.org>
2015-05-03 17:07:58 +0200

    Indentation

--- typer.ml
+++ typer.ml
@@ -19,13 +19,13 @@ exception Arity_mismatch of expr * expr list;;
 
 (* Options to pass to the type checking and type inference functions *)
 type opts =
-    {
-      default_type : expr;      (* A type to associate to free variables *)
-      scope_warnings : bool;    (* Should we warn when we encounter free variables? *)
-      undeclared_functions_warning : bool; (* Should we warn when we encounter undeclared function symbols? *)
-      register_new_constants : bool; (* If we infer the type of an unknown function symbol, should we register it? *)
-      fully_type : bool;             (* Should everything be typed? *)
-    }
+  {
+    default_type : expr;      (* A type to associate to free variables *)
+    scope_warnings : bool;    (* Should we warn when we encounter free variables? *)
+    undeclared_functions_warning : bool; (* Should we warn when we encounter undeclared function symbols? *)
+    register_new_constants : bool; (* If we infer the type of an unknown function symbol, should we register it? *)
+    fully_type : bool;             (* Should everything be typed? *)
+  }
 ;;
 
 (* A global hashtable of constant values.
@@ -132,100 +132,100 @@ let rec infer_expr opts env = function
      elam (x, infer_expr opts (x :: env) body)
   | _ -> assert false
 and xcheck_expr opts env ty e =
-    if ty == type_none
-    then infer_expr opts env e
-    else
-      match e with
-      | Evar (s, _) ->
-         (* Call inference because it checks for scoping *)
-         let e' = infer_expr opts env e in
-         let ret_ty = get_type e' in
-         if ret_ty == ty then
-           e'
+  if ty == type_none
+  then infer_expr opts env e
+  else
+    match e with
+    | Evar (s, _) ->
+       (* Call inference because it checks for scoping *)
+       let e' = infer_expr opts env e in
+       let ret_ty = get_type e' in
+       if ret_ty == ty then
+         e'
+       else
+         raise (Type_Mismatch (ret_ty, ty, "Typer.xcheck_expr"))
+    | Emeta (Eall (Evar (s, _), b, _), _) ->
+       emeta (check_expr opts env type_prop (eall (tvar s ty, b)))
+    | Eapp (Evar ("=", _) as eq, [e1; e2], _) ->
+       (* Equality is the only symbol for which
+          implicit polymorphism is allowed. *)
+       (* First try to infer the type of e1 *)
+       let e1' = infer_expr opts env e1 in
+       let t1 = get_type e1' in
+       (* If this fails, try e2 *)
+       if t1 == type_none then
+         let e2' = infer_expr opts env e2 in
+         let t2 = get_type e2' in
+         (* if it also fails, finally look at the type annotation of the equality constant *)
+         if t2 == type_none then
+           match get_type eq with
+           | Earrow ([t1; t2], prop, _) when t1 == t2 && prop = type_prop ->
+              eeq (check_expr opts env t1 e1) (check_expr opts env t2 e2)
+           | _ ->                    (* We hav no more clue to guess the type of this equality *)
+              eeq e1' e2'
          else
-           raise (Type_Mismatch (ret_ty, ty, "Typer.xcheck_expr"))
-      | Emeta (Eall (Evar (s, _), b, _), _) ->
-         emeta (check_expr opts env type_prop (eall (tvar s ty, b)))
-      | Eapp (Evar ("=", _) as eq, [e1; e2], _) ->
-         (* Equality is the only symbol for which
-            implicit polymorphism is allowed. *)
-         (* First try to infer the type of e1 *)
-         let e1' = infer_expr opts env e1 in
-         let t1 = get_type e1' in
-         (* If this fails, try e2 *)
-         if t1 == type_none then
-           let e2' = infer_expr opts env e2 in
-           let t2 = get_type e2' in
-           (* if it also fails, finally look at the type annotation of the equality constant *)
-           if t2 == type_none then
-             match get_type eq with
-             | Earrow ([t1; t2], prop, _) when t1 == t2 && prop = type_prop ->
-                eeq (check_expr opts env t1 e1) (check_expr opts env t2 e2)
-             | _ ->                    (* We hav no more clue to guess the type of this equality *)
-                eeq e1' e2'
-           else
-             eeq (check_expr opts env t2 e1) e2'
-         else
-           eeq e1' (check_expr opts env t1 e2)
-      | Eapp (Evar (f_sym, _) as f, args, _) ->
-         (* First lookup in the constant table *)
-         (* Remark: If it fails, it looks at it on the symbol. *)
-         let tyf = get_type (type_const opts f) in
-         if tyf == type_none
-         then
-          (Log.debug 15 "Check undeclared symbol %s" f_sym;
-           let typed_args = List.map (infer_expr opts env) args in
-           let args_types = List.map get_type typed_args in
-           let infered_const_type = earrow args_types ty in
-           if opts.register_new_constants then
-             declare_constant (f_sym, infered_const_type);
-           eapp (tvar f_sym infered_const_type, typed_args))
+           eeq (check_expr opts env t2 e1) e2'
+       else
+         eeq e1' (check_expr opts env t1 e2)
+    | Eapp (Evar (f_sym, _) as f, args, _) ->
+       (* First lookup in the constant table *)
+       (* Remark: If it fails, it looks at it on the symbol. *)
+       let tyf = get_type (type_const opts f) in
+       if tyf == type_none
+       then
+         (Log.debug 15 "Check undeclared symbol %s" f_sym;
+          let typed_args = List.map (infer_expr opts env) args in
+          let args_types = List.map get_type typed_args in
+          let infered_const_type = earrow args_types ty in
+          if opts.register_new_constants then
+            declare_constant (f_sym, infered_const_type);
+          eapp (tvar f_sym infered_const_type, typed_args))
+       else
+         (Log.debug 15 "Check declared symbol %s : %a" f_sym Print.pp_expr tyf;
+          let (type_args, term_args, tys, ret_ty) = instantiate args tyf in
+          if ret_ty == ty
+          then
+            eapp (tvar f_sym tyf, type_args @ List.map2 (check_expr opts env) tys term_args)
           else
-          (Log.debug 15 "Check declared symbol %s : %a" f_sym Print.pp_expr tyf;
-           let (type_args, term_args, tys, ret_ty) = instantiate args tyf in
-           if ret_ty == ty
-           then
-             eapp (tvar f_sym tyf, type_args @ List.map2 (check_expr opts env) tys term_args)
-           else
-             raise (Type_Mismatch (ret_ty, ty, "Typer.xcheck_expr")))
-      | Enot (e, _) ->
-         assert (ty == type_prop);
-         enot (check_expr opts env type_prop e)
-      | Eand (a, b, _) ->
-         assert (ty == type_prop);
-         eand (check_expr opts env type_prop a, check_expr opts env type_prop b)
-      | Eor (a, b, _) ->
-         assert (ty == type_prop);
-         eor (check_expr opts env type_prop a, check_expr opts env type_prop b)
-      | Eimply (a, b, _) ->
-         assert (ty == type_prop);
-         eimply (check_expr opts env type_prop a, check_expr opts env type_prop b)
-      | Eequiv (a, b, _) ->
-         assert (ty == type_prop);
-         eequiv (check_expr opts env type_prop a, check_expr opts env type_prop b)
-      | Etrue -> assert (ty == type_prop); etrue
-      | Efalse -> assert (ty == type_prop); efalse
-      | Eall (Evar _ as x, body, _) ->
-         assert (ty == type_prop);
-         eall (x, check_expr opts (x :: env) type_prop body)
-      | Eex (Evar _ as x, body, _) ->
-         assert (ty == type_prop);
-         eex (x, check_expr opts (x :: env) type_prop body)
-      | Etau (Evar _ as x, body, _) ->
-         let newx = check_expr opts env x ty in
-         etau (newx, check_expr opts (newx :: env) type_prop body)
-      | Elam (Evar _ as x, body, _) ->
-         (
-           let mk_arrow l ret =
-             if l = [] then ret else earrow l ret
-           in
-           match ty with
-           | Earrow (hd :: tl, ret, _) ->
-              let newx = check_expr opts env hd x in
-              elam (newx, check_expr opts (x :: env) (mk_arrow tl ret) body)
-           | _ -> raise (Invalid_argument "Typer.check_expr: arrow expected")
-         )
-      | _ -> assert false
+            raise (Type_Mismatch (ret_ty, ty, "Typer.xcheck_expr")))
+    | Enot (e, _) ->
+       assert (ty == type_prop);
+       enot (check_expr opts env type_prop e)
+    | Eand (a, b, _) ->
+       assert (ty == type_prop);
+       eand (check_expr opts env type_prop a, check_expr opts env type_prop b)
+    | Eor (a, b, _) ->
+       assert (ty == type_prop);
+       eor (check_expr opts env type_prop a, check_expr opts env type_prop b)
+    | Eimply (a, b, _) ->
+       assert (ty == type_prop);
+       eimply (check_expr opts env type_prop a, check_expr opts env type_prop b)
+    | Eequiv (a, b, _) ->
+       assert (ty == type_prop);
+       eequiv (check_expr opts env type_prop a, check_expr opts env type_prop b)
+    | Etrue -> assert (ty == type_prop); etrue
+    | Efalse -> assert (ty == type_prop); efalse
+    | Eall (Evar _ as x, body, _) ->
+       assert (ty == type_prop);
+       eall (x, check_expr opts (x :: env) type_prop body)
+    | Eex (Evar _ as x, body, _) ->
+       assert (ty == type_prop);
+       eex (x, check_expr opts (x :: env) type_prop body)
+    | Etau (Evar _ as x, body, _) ->
+       let newx = check_expr opts env x ty in
+       etau (newx, check_expr opts (newx :: env) type_prop body)
+    | Elam (Evar _ as x, body, _) ->
+       (
+         let mk_arrow l ret =
+           if l = [] then ret else earrow l ret
+         in
+         match ty with
+         | Earrow (hd :: tl, ret, _) ->
+            let newx = check_expr opts env hd x in
+            elam (newx, check_expr opts (x :: env) (mk_arrow tl ret) body)
+         | _ -> raise (Invalid_argument "Typer.check_expr: arrow expected")
+       )
+    | _ -> assert false
 and check_expr opts env ty e =
   Log.debug 15 "Check %a : %a" Print.pp_expr e Print.pp_expr ty;
   let result = xcheck_expr opts env ty e in
