0626e09343d2605a080f9d4b95033de6b19e5157
Raphaël Cauderlier <cauderlier@crans.org>
2015-11-09 16:42:29 +0100

    Missing declaration

--- dkterm.mli
+++ dkterm.mli
@@ -117,6 +117,7 @@ val mk_rwrt             : dkterm list * dkterm * dkterm -> line
 
 val print_line          : out_channel -> line -> unit
 val print_goal_type     : out_channel -> string -> dkterm -> unit
+val print_goal_def      : out_channel -> dkterm -> unit
 val print_dk_type       : out_channel -> dkterm -> unit
 val print_dk_term       : out_channel -> dkterm -> unit
 val print_proof         : out_channel -> string -> dkterm -> unit
