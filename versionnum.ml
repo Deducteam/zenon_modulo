e147c5568215e54cef9320fec50845a59947545b
Raphael Cauderlier <cauderlier@liafa.univ-paris-diderot.fr>
2017-03-26 13:18:40 +0200

    Bump version number to follow Opam versionning

--- versionnum.ml
+++ versionnum.ml
@@ -8,7 +8,7 @@ let date = "2015-09-01";;
 
 let major = 0;;
 let minor = 4;;
-let bugfix = 2;;
+let bugfix = 3;;
 
 let short = sprintf "%d.%d.%d" major minor bugfix;;
 let full = sprintf "%d.%d.%d [a%d] %s" major minor bugfix number date;;
